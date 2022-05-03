baa0b98e18ba30c6e864a32489836de4fefe35d4
Michael <msiller455@gmail.com>
2022-05-03 15:20:04 -0700

    MERN-stack Infrastructure

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service'
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import NavBar from '../../components/NavBar/NavBar'
 import './App.css';
 
 export default function App() {
+  const [user, setUser] = useState(getUser())
+
   return (
     <main className="App">
-      App
+      {
+        user ?
+        <>
+          <NavBar user={user} />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />}/>
+            <Route path="/orders" element={<OrderHistoryPage />}/>
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
     </main>
   );
 }
